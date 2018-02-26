164e124e9ec21a483953a197c74113d90e5b3db4
BrookeAFoote <bfoote@tulane.edu>
2018-02-26 13:35:57 -0800

    testing

--- routes.js
+++ routes.js
@@ -2,22 +2,31 @@ var express = require("express");
 var router = express.Router();
 var inMemoryDatabase = require("./in-memory-database");
 
-router.get("/tasks", function(req, res){
+var toDoTask = inMemoryDatabase();
+toDoTask.init([
+    {task: "walk the dog"}
+]);//todotask
 
+router.get("/tasks", function(req, res){
+    res.send(toDoTask.readAll());
 
 }); //routerget
 
 router.post("/tasks", function(req, res){
-
+    var task = req.body;
+    toDoTask.create(task);
+    res.status(201).send(task)
 
 });//routerpost
 
 router.put("/tasks", function(req, res){
-
+    res.send("update");
 });//routerput
 
 router.delete("/tasks", function(req, res){
-
+    var id = req.params.id;
+    toDoTask.delete(id);
+    res.send("deleted");
 });//routerdelete
 
 
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var studentRoutes = require("./routes");
+var Routes = require("./routes");
 var app = express();
 app.use(bodyParser.json());
 app.use("/", Routes);
